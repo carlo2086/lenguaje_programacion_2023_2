af77df6c46a9e07e8351fd8ad8eadcfe74063207
carlo2086 <rivinos2086@hotmail.com>
2023-12-28 09:58:40 -0500

    bucles

--- sesion_02/bucle_for_each.cpp
+++ sesion_02/bucle_for_each.cpp
@@ -3,9 +3,11 @@ using namespace std;
 
 int main()
 {
+    int array_int[] = {1,2,3,4,5,6};
+
     for (int i = 0; i <= 5; i++)
     {
-        cout << i << "+ \n";
+        cout << array_int[i] << "+ \n";
     }
     return 0;
 }
