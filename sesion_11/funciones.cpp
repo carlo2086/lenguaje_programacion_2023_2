e29d753dd50522a2626835ce289e4d0f7696eb9e
carlo2086 <rivinos2086@hotmail.com>
2024-02-15 08:55:34 -0500

    sesion 11 funciones

---
+++ sesion_11/funciones.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int num1, num2;
+void suma(int numero_1, int numero_2);
+void ingreso_valores();
+
+int main()
+{
+    cout << "Ingrese los numeros a sumar" << endl;
+    ingreso_valores();
+    suma(num1, num2);
+    return 0;
+}
+
+void suma(int numero_1, int numero_2)
+{
+    // return numero_1 + numero_2;
+    cout << "la suma es: " << numero_1 + numero_2 << endl;
+}
+
+void ingreso_valores()
+{
+    cout << "Ingrese primer numero" << endl;
+    cin >> num1;
+    cout << "Ingrese segundo numero" << endl;
+    cin >> num2;
+}
