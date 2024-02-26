05fb88fd5563f84f612e14aed91e8c4b382d554b
carlo2086 <rivinos2086@hotmail.com>
2024-02-26 17:06:22 -0500

    herencia

--- sesion_13/main_carro.cpp
+++ sesion_13/main_carro.cpp
@@ -7,6 +7,8 @@ int main(){
 
     CarroVan carro_van(1,16,"1500 cc","2.0 petrolero",1,"Van",2024,"Rosa","Hyundai");
     cout<<carro_van.getModelo();
+
+    cout<<carro_van.getAñoFabricacion();
     
     return 0;
 }
